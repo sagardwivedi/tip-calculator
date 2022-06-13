384f438013d49427c1ff0fd3ea8e31251a5fe962
sagardwivedi <Sagar.dw20@gmail.com>
2022-06-13 14:54:25 +0530

    Something good

---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+import { NextPage } from "next";
+import { Head, Html, Main, NextScript } from "next/document";
+
+const Document: NextPage = () => {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default Document;
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      space_mono: ["Space Mono", "sans-serif"],
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: "#000",
+      white: "#fff",
+      cyan: {
+        500: "#26c0ab",
+        900: "#00494d",
+      },
+      grayish_cyan: {
+        100: "#f4fafa",
+        300: "#c5e4e7",
+        500: "#7f9c9f",
+        700: "#5e7a7d",
+      },
+    },
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
