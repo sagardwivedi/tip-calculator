1ee6173173e27138ae5f4d1ca79e169d62e1899b
sagardwivedi <Sagar.dw20@gmail.com>
2022-06-14 13:01:34 +0530

    Refactoring

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,49 @@
 import { NextPage } from "next";
 import { useState } from "react";
 
+const Index: NextPage = () => {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="flex flex-col items-center justify-center">
+        <h3 className="mb-12 tracking-[0.6rem] text-grayish_cyan-700">
+          <span>SPLI</span>
+          <br />
+          <span>TTER</span>
+        </h3>
+        <div className="flex w-full max-w-4xl flex-row flex-wrap justify-center gap-y-10 rounded-t-[2rem] bg-white p-8 sm:gap-x-10 sm:rounded-2xl sm:p-6">
+          <div>
+            <BillInput />
+            <TipInput />
+            <NumberOfPeopleInput />
+          </div>
+          <div className="flex flex-grow flex-col justify-between rounded-2xl bg-cyan-900 px-8 py-10">
+            <div className="space-y-6 sm:w-60">
+              <TipAmount />
+              <TotalAmount />
+            </div>
+            <button
+              className="mt-6 w-full cursor-pointer rounded-sm border-none bg-cyan-500/90 py-2 text-cyan-900 disabled:bg-cyan-900/70"
+              type="reset"
+            >
+              RESET
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export const BillInput: NextPage = () => {
-  // assign value type to state
   const [bill, setBill] = useState<number>(0);
   return (
-    <div className="relative flex flex-col">
+    <div className="relative mb-8">
       <p>Bill</p>
       <input
         type={"number"}
-        className="input-style"
-        placeholder="0"
+        className={`input-style ${
+          bill > 0 ? "text-cyan-900" : "text-grayish_cyan-500"
+        }`}
         value={bill}
         onChange={(e) => setBill(Number(e.target.value))}
       />
@@ -34,21 +67,30 @@ export const BillInput: NextPage = () => {
 };
 
 export const TipInput: NextPage = () => {
+  const [tip, setTip] = useState<string>("");
   return (
-    <div className="relative flex flex-col">
+    <div className="mb-8">
       <p>Select Tip %</p>
-      <div className="grid grid-cols-2 gap-5 sm:grid-cols-3">
-        {["5%", "10%", "15%", "25%", "50%", "Custom"].map((number) => (
-          <button
-            className={`cursor-pointer rounded-md border-none p-1 text-xl text-grayish_cyan-300 ${
-              number === "Custom"
-                ? "bg-grayish_cyan-100 text-grayish_cyan-700"
-                : "bg-cyan-900"
-            }`}
-            key={number}
-          >
-            {number}
-          </button>
+      <div className="grid grid-cols-2 gap-4 sm:grid-cols-3">
+        {["5%", "10%", "15%", "25%", "50%", "Custom"].map((tip) => (
+          <label key={tip}>
+            <input
+              className="peer sr-only"
+              name="size"
+              type="radio"
+              value={tip}
+              onChange={(e) => setTip(e.target.value)}
+            />
+            <div
+              className={`${
+                tip === "Custom"
+                  ? "bg-grayish_cyan-100 text-grayish_cyan-700"
+                  : "bg-cyan-900 text-grayish_cyan-300"
+              } flex cursor-pointer items-center justify-center rounded-md  py-2  peer-checked:bg-cyan-500 peer-checked:text-cyan-900`}
+            >
+              {tip}
+            </div>
+          </label>
         ))}
       </div>
     </div>
@@ -56,10 +98,19 @@ export const TipInput: NextPage = () => {
 };
 
 export const NumberOfPeopleInput: NextPage = () => {
+  const [numberOfPeople, setNumberOfPeople] = useState<number>(0);
+
   return (
-    <div className="relative flex flex-col">
+    <div className="relative">
       <p>Number of People</p>
-      <input type={"number"} className="input-style" placeholder="0" />
+      <input
+        type={"number"}
+        value={numberOfPeople}
+        onChange={(e) => setNumberOfPeople(Number(e.target.value))}
+        className={`input-style ${
+          numberOfPeople > 0 ? "text-cyan-900" : "text-grayish_cyan-500"
+        }`}
+      />
       <span className="svg-style">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -80,10 +131,10 @@ export const NumberOfPeopleInput: NextPage = () => {
 
 export const TipAmount: NextPage = () => {
   return (
-    <div className="flex flex-row items-center justify-between">
+    <div className="flex flex-row  items-center justify-between">
       <h5 className="text-white">
         Tip Amount <br />
-        <span className="text-grayish_cyan-500">/ person</span>
+        <span className="text-grayish_cyan-500">/person</span>
       </h5>
       <span className="text-4xl font-bold text-cyan-500">$0.00</span>
     </div>
@@ -95,39 +146,9 @@ export const TotalAmount: NextPage = () => {
     <div className="flex flex-row items-center justify-between">
       <h5 className="text-white">
         Total <br />
-        <span className="text-grayish_cyan-500">/ person</span>
+        <span className="text-grayish_cyan-500">/person</span>
       </h5>
       <span className="text-4xl font-bold text-cyan-500">$0.00</span>
-    </div>
-  );
-};
-
-const Index: NextPage = () => {
-  return (
-    <div className="flex h-screen flex-col justify-end sm:flex-row sm:justify-center">
-      <div className="flex max-w-4xl flex-col items-center justify-center">
-        <h3 className="mb-12 tracking-[0.6rem] text-grayish_cyan-700">
-          <span>SPLI</span>
-          <br />
-          <span>TTER</span>
-        </h3>
-        <div className="flex flex-row flex-wrap items-stretch justify-center gap-y-10 rounded-2xl bg-white p-6 sm:gap-x-10">
-          <div className="">
-            <BillInput />
-            <TipInput />
-            <NumberOfPeopleInput />
-          </div>
-          <div className="grow rounded-2xl bg-cyan-900 px-6 py-8">
-            <div>
-              <TipAmount />
-              <TotalAmount />
-              <button className="w-full" type="reset">
-                RESET
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   );
 };
