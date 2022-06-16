0561e56591716e7a008038adc2bd510563db7e18
sagardwivedi <Sagar.dw20@gmail.com>
2022-06-16 11:27:15 +0530

    Bug: Fixed  checked and unchecked state of radio button

--- components/TipInput.tsx
+++ components/TipInput.tsx
@@ -1,32 +1,34 @@
 import { NextPage } from "next";
 
 type TipInputProps = {
+  tip: number;
   setTip: (tip: number) => void;
 };
 
-const TipInput: NextPage<TipInputProps> = ({ setTip }) => {
+const TipInput: NextPage<TipInputProps> = ({ tip, setTip }) => {
   return (
     <div className="mb-8">
       <p className="mb-3">Select Tip %</p>
-      <div className="flex flex-row flex-wrap gap-4 sm:grid sm:grid-cols-3 sm:gap-4">
-        {[5, 10, 15, 25, 50].map((tip) => (
-          <label key={tip}>
+      <div className="flex flex-row flex-wrap gap-[10px] sm:grid sm:grid-cols-3">
+        {[5, 10, 15, 25, 50].map((value) => (
+          <label key={value}>
             <input
               className="peer sr-only"
               name="size"
               type="radio"
-              value={tip}
+              value={value}
               onChange={(e) => setTip(Number(e.target.value))}
+              checked={tip === 0 ? false : tip === value}
             />
             <div
               className={`flex h-11 w-[9.5rem] cursor-pointer items-center justify-center rounded-md bg-cyan-900 text-xl text-grayish_cyan-300 transition-colors duration-200 ease-linear peer-checked:bg-cyan-500 peer-checked:text-cyan-900 sm:w-28`}
             >
-              {tip}%
+              {value}%
             </div>
           </label>
         ))}
         <input
-          className={`h-12 w-[9.5rem] rounded-md border-none bg-grayish_cyan-100 text-right text-2xl text-cyan-900 placeholder:text-center focus-within:ring-2 focus-within:ring-cyan-500 sm:w-28`}
+          className={`h-11 w-[9.5rem] rounded-md border-none bg-grayish_cyan-100 text-right text-xl text-cyan-900 placeholder:text-center focus-within:ring-2 focus-within:ring-cyan-500 sm:w-28`}
           type={"number"}
           placeholder={`Custom`}
         />
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,6 @@ const Index: NextPage = () => {
 
   const calculateResult = () => {
     if (bill > 0 && tip > 0 && numberOfPeople > 0) {
-      // formula to calculate tip
       const tipAmount = (bill * tip) / 100;
       const tipPerPerson = tipAmount / numberOfPeople;
       const personAmount = (tipAmount + bill) / numberOfPeople;
@@ -39,6 +38,11 @@ const Index: NextPage = () => {
     <div className="mt-[6%] flex justify-center">
       <Head>
         <title>Tip Calculator</title>
+        {/* give information about app */}
+        <meta
+          name="description"
+          content="A wonderfull app to calculate the tip. BestUI"
+        />
       </Head>
       <div className="flex flex-col items-center justify-center">
         <h3 className="mb-8 text-2xl tracking-[0.6rem] text-grayish_cyan-700 sm:mb-12 sm:text-lg">
@@ -47,15 +51,15 @@ const Index: NextPage = () => {
           <span>TTER</span>
         </h3>
         <div className="flex flex-row flex-wrap justify-center gap-y-10 rounded-t-[2rem] bg-white p-8 sm:gap-x-10 sm:rounded-2xl sm:p-6">
-          <div>
+          <div className="flex-grow">
             <BillInput bill={bill} setBill={setBill} />
-            <TipInput setTip={setTip} />
+            <TipInput tip={tip} setTip={setTip} />
             <NumberOfPeopleInput
               numberOfPeople={numberOfPeople}
               setNumberOfPeople={setNumberOfPeople}
             />
           </div>
-          <div className="flex flex-grow flex-col justify-between  rounded-2xl bg-cyan-900 px-8 py-10">
+          <div className="flex grow flex-col justify-between rounded-2xl bg-cyan-900 px-8 py-10">
             <ResultAmount
               tipAmount={tipPerPerson}
               personAmount={personAmount}
